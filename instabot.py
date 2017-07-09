aedf3698ba002cf328db99f75a9eb229c8eee5ec
vik7am <vik7am@gmail.com>
2017-07-09 16:18:19 +0530

    added get recent media liked by user

--- instabot.py
+++ instabot.py
@@ -1,11 +1,10 @@
 import requests, urllib
 from token import ACCESS_TOKEN
 
-BASE_URL = 'https://api.instagram.com/v1/'
+BASE_URL = "https://api.instagram.com/v1/"
 
 def get_self_info():
     url = BASE_URL + "users/self/?access_token=" + ACCESS_TOKEN
-    print "URL: "+url
     self_info = requests.get(url).json()
 
     if self_info["meta"]["code"] == 200:
@@ -22,9 +21,7 @@ def get_self_info():
 
 def get_user_id(username):
     url = BASE_URL + "users/search?q="+username+"&access_token=" +ACCESS_TOKEN
-    #print "URL:"+url
     user_info = requests.get(url).json()
-    #print user_info
     if user_info["meta"]["code"] == 200:
         if len(user_info["data"]):
             return user_info["data"][0]["id"]
@@ -35,35 +32,66 @@ def get_user_id(username):
 
 def get_self_post():
     url = BASE_URL + "users/self/media/recent/?access_token="+ ACCESS_TOKEN
-    print "URL:" + url
-    own_media = requests.get(url).json()
-    s_no=1
-    for media in own_media["data"]:
-        caption = "No Caption"
-        if media["caption"] != None:
-            caption = media["caption"]["text"]
-        print str(s_no) + ". " + media["id"] + " " + str(caption)
-        s_no += 1
+    self_media = requests.get(url).json()
+    
+    if self_media["meta"]["code"] == 200:
+        if len(self_media["data"]):
+            image_id = self_media["data"][0]["id"]
+            image_url = self_media["data"][0]["images"]["standard_resolution"]["url"]
+            urllib.urlretrieve(image_url, image_id + ".jpeg")
+            return image_id
+        else:
+            print "Post does not exist!"
+            return 0
+    else:
+        print "Status code other than 200 received!"
+        return 0
+    
 
 def get_other_post(user_id):
     url = BASE_URL + "users/"+user_id+"/media/recent/?access_token="+ ACCESS_TOKEN
-    print "URL:" + url
-    own_media = requests.get(url).json()
-    s_no=1
-    for media in own_media["data"]:
-        caption="No Caption"
-        if media["caption"]!=None:
-            caption = media["caption"]["text"]
-        print str(s_no)+". "+media["id"]+" "+str(caption)
-        s_no+=1
+    self_media = requests.get(url).json()
+
+    if self_media["meta"]["code"] == 200:
+        if len(self_media["data"]):
+            image_id = self_media["data"][0]["id"]
+            image_url = self_media["data"][0]["images"]["standard_resolution"]["url"]
+            urllib.urlretrieve(image_url, image_id + ".jpeg")
+            return image_id
+        else:
+            print "Post does not exist!"
+            return 0
+    else:
+        print "Error" + self_media["meta"]["code"]
+        return 0
+
+def get_media_likes():
+    url = BASE_URL + "users/self/media/liked/?access_token=" + ACCESS_TOKEN
+    self_media = requests.get(url).json()
+
+    if self_media["meta"]["code"] == 200:
+        if len(self_media["data"]):
+            image_id = self_media["data"][0]["id"]
+            image_url = self_media["data"][0]["images"]["standard_resolution"]["url"]
+            urllib.urlretrieve(image_url, image_id + ".jpeg")
+            return image_id
+        else:
+            print "No Recent liked Media found"
+            return 0
+    else:
+        print "Error" + self_media["meta"]["code"]
+        return 0
+
 
 def home():
     running=True
+
     while running:
         print "1.Get Self info "
         print "2.Get Other User ID"
         print "3.Get Self Post"
         print "4.Get Other Post"
+        print "5.Get recent media liked by the user"
         choice=raw_input("")
         if choice=="1":
             get_self_info()
@@ -71,9 +99,15 @@ def home():
             user=raw_input("Enter Username:")
             print str(get_user_id(user))
         elif choice=="3":
-            get_self_post()
+            image_id=get_self_post()
+            print "Downloaded "+str(image_id)+".jpg"
         elif choice=="4":
-            get_other_post(get_user_id("instabot.mriu.test.8"))
+            user_id=raw_input("Enter UserName: ")
+            image_id = get_other_post(get_user_id(user_id))
+            print "Downloaded " + str(image_id) + ".jpg"
+        elif choice == "5":
+            id=get_media_likes()
+            print "Downloaded " + str(id) + ".jpg"
         else:
             running=False
         print "\n"
