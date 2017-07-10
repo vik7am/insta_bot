10c2e9967949fc66368b6a7160f9f957c6da3abb
vik7am <vik7am@gmail.com>
2017-07-10 13:46:11 +0530

    finding hashtag in captions

--- hashtag_manager.py
+++ hashtag_manager.py
@@ -3,21 +3,46 @@ from token import ACCESS_TOKEN
 from id_manager import get_user_id
 BASE_URL = "https://api.instagram.com/v1/"
 
-def find_hashtag(user_id,hashtag):
+HASHTAG=[]
+CAPTION=[]
+def find_hashtag(user_id):
     counter=0
     url = BASE_URL + "users/" + user_id + "/media/recent/?access_token=" + ACCESS_TOKEN
-    self_media = requests.get(url).json()
-    if self_media["meta"]["code"] == 200:
-        for media in self_media["data"]:
+    user_media = requests.get(url).json()
+    if user_media["meta"]["code"] == 200:
+        #length=len(user_media["data"])
+        for media in user_media["data"]:
             caption=media["caption"]["text"]
-            if str(caption).find(hashtag)>=0:
-                counter+=1
+            CAPTION.append(caption)
+
+        for caption in CAPTION:
+            WORD=str(caption).split(" ")
+            for word in WORD:
+                if str(word).find("#") != -1:
+                    if find_duplicates(word):
+                        HASHTAG.append(word)
+
+
+        sno=1
+        for h in HASHTAG:
+            print str(sno)+h
+            sno+=1
         return counter
     else:
-        print "Error" + self_media["meta"]["code"]
+        print "Error" + user_media["meta"]["code"]
         return -1
 
-def get_image_with_hashtag(username,hashtag):
+def find_duplicates(word):
+    for hashtag in HASHTAG:
+        if hashtag == word:
+            return False
+    return True
+
+
+
+
+
+def get_image_with_hashtag(username):
     user_id=get_user_id(username)
-    no_of_images=find_hashtag(user_id,hashtag)
-    print "No of images with "+hashtag+" : "+str(no_of_images)
+    find_hashtag(user_id)
+    #print "No of images with "+hashtag+" : "+str(no_of_images)
