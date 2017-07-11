3e3ff69e9ce98ed4aca1e752dc37d4dd5e53355c
vik7am <vik7am@gmail.com>
2017-07-11 16:09:55 +0530

    added function to filter post by criteria

--- instabot.py
+++ instabot.py
@@ -1,5 +1,5 @@
 from id_manager import get_self_info,get_user_id
-from post_manager import get_self_post,get_other_post,get_media_liked
+from post_manager import get_self_post,get_other_post,get_media_liked,get_other_post_by_criteria
 from lc_manager import like_a_post,get_comment_list,post_a_comment
 from hashtag_manager import get_image_with_hashtag
 BASE_URL = "https://api.instagram.com/v1/"
@@ -10,13 +10,15 @@ def home():
         print "1.Get Self info "
         print "2.Get Other User ID"
         print "3.Get Self Post"
-        print "4.Get Other Post"
-        print "5.Get recent media liked by the user"
-        print "6.Like a Post"
-        print "7.Get List of comments"
-        print "8.Comment on a Post"
-        print "9.Get no of post with hash tag"
+        print "4.Get Other user Post"
+        print "5.Get Other user post by criteria"
+        print "6.Get recent media liked by the user"
+        print "7.Like a Post"
+        print "8.Get List of comments"
+        print "9.Comment on a Post"
+        print "10.Get no of post with hash tag"
         print "0.Exit"
+
         choice=raw_input("")
         if choice=="1":
             get_self_info()
@@ -40,26 +42,32 @@ def home():
                 print "Downloaded " + str(image_id) + ".jpg"
 
         elif choice == "5":
+            username = raw_input("Enter UserName: ")
+            user_id = get_user_id(username)
+            get_other_post_by_criteria(user_id)
+
+        elif choice == "6":
             image_id=get_media_liked()
             if image_id != -1:
                 print "Downloaded " + str(image_id) + ".jpg"
 
-        elif choice == "6":
+        elif choice == "7":
             username = raw_input("Enter UserName: ")
             like_a_post(username)
 
-        elif choice == "7":
+        elif choice == "8":
             username = raw_input("Enter UserName: ")
             get_comment_list(username)
 
-        elif choice == "8":
+        elif choice == "9":
             username = raw_input("Enter UserName: ")
             post_a_comment(username)
 
-        elif choice == "9":
+        elif choice == "10":
             username = raw_input("Enter UserName: ")
-            #hashtag = raw_input("Enter Hashtag: ")
             get_image_with_hashtag(username)
+
+
 
         else:
             running=False
--- post_manager.py
+++ post_manager.py
@@ -1,4 +1,5 @@
 import requests, urllib
+import os
 from mytoken import ACCESS_TOKEN
 BASE_URL = "https://api.instagram.com/v1/"
 
@@ -7,7 +8,6 @@ def get_self_post():
     url = BASE_URL + "users/self/media/recent/?access_token="+ ACCESS_TOKEN
     self_media = requests.get(url).json()
     if self_media["meta"]["code"] == 200:
-        print len(self_media["data"])
         if len(self_media["data"]):
             image_id = self_media["data"][0]["id"]
             image_url = self_media["data"][0]["images"]["standard_resolution"]["url"]
@@ -18,6 +18,22 @@ def get_self_post():
             return -1
     else:
         print "Status code other than 200 received!"
+        return -1
+
+def get_media_liked():
+    url = BASE_URL + "users/self/media/liked/?access_token=" + ACCESS_TOKEN
+    self_media = requests.get(url).json()
+    if self_media["meta"]["code"] == 200:
+        if len(self_media["data"]):
+            image_id = self_media["data"][0]["id"]
+            image_url = self_media["data"][0]["images"]["standard_resolution"]["url"]
+            urllib.urlretrieve(image_url, image_id + ".jpeg")
+            return image_id
+        else:
+            print "No Recent liked Media found"
+            return -1
+    else:
+        print "Error" + self_media["meta"]["code"]
         return -1
 
 def get_other_post(user_id):
@@ -36,18 +52,37 @@ def get_other_post(user_id):
         print "Error" + self_media["meta"]["code"]
         return -1
 
-def get_media_liked():
-    url = BASE_URL + "users/self/media/liked/?access_token=" + ACCESS_TOKEN
-    self_media = requests.get(url).json()
-    if self_media["meta"]["code"] == 200:
-        if len(self_media["data"]):
-            image_id = self_media["data"][0]["id"]
-            image_url = self_media["data"][0]["images"]["standard_resolution"]["url"]
-            urllib.urlretrieve(image_url, image_id + ".jpeg")
-            return image_id
-        else:
-            print "No Recent liked Media found"
-            return -1
+def get_all_media(user_id,criteria,value):
+    ID=[]
+    url = BASE_URL + "users/" + user_id + "/media/recent/?access_token=" + ACCESS_TOKEN
+    user_media = requests.get(url).json()
+    if user_media["meta"]["code"] == 200:
+        for media in user_media["data"]:
+            if criteria == "1":
+                if media["likes"]["count"]>=int(value):
+                    ID.append(media["id"])
+                    urllib.urlretrieve(media["images"]["standard_resolution"]["url"], media["id"] + ".jpeg")
+            else:
+                if str(media["caption"]).find(value) != -1:
+                    ID.append(media["id"])
+                    urllib.urlretrieve(media["images"]["standard_resolution"]["url"], media["id"] + ".jpeg")
+        print "Name of downloaded images"
+        for id in ID:
+            print str(id)+".jpg"
+
     else:
-        print "Error" + self_media["meta"]["code"]
+        print "Error" + user_media["meta"]["code"]
         return -1
+
+
+
+def get_other_post_by_criteria(user_id):
+    print "1.Get post by min no of likes"
+    print "2.Get post by text in caption"
+    criteria=raw_input("")
+    if criteria == "1":
+        value=raw_input("Enter minimum no of likes: ")
+        get_all_media(user_id,criteria,value)
+    elif criteria == "2":
+        value=raw_input("Enter text: ")
+        get_all_media(user_id, criteria,value)
